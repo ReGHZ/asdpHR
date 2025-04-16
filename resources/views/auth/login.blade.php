5ea2d005042257d8cf7c7288f0a2733e71b5cc35
ReGHZ <satriateguhghz@gmail.com>
2025-04-16 09:32:12 +0700

    fix image

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -72,7 +72,7 @@
             </div>
             <div class="col-lg-7 d-none d-lg-block">
                 <div id="auth-right" style="max-height: 100vh; overflow:hidden; filter: brightness(50%);">
-                    <img src="/public/backend/assets/images/asdp.jpg">
+                    <img src="{{ asset('/backend/assets/images/asdp.jpg') }}">
                 </div>
             </div>
         </div>
